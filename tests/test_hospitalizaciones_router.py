d3fb37198b44b3cbf67bb727726d380a340d4d19
Juan David <jdroserot@udistrital.edu.co>
2026-05-10 10:04:00 -0500

    Add unit tests

--- tests/test_hospitalizaciones_router.py
+++ tests/test_hospitalizaciones_router.py
@@ -23,3 +23,60 @@ def test_crear_hospitalizacion_exitoso(client,borrar_hospitalizaciones):
     })
     assert response.status_code == 201
     assert response.json()["data"]["id_urgencia"] == 50
+
+def test_crear_atencion_hospitalizacion_doctor_no_existe(client, crear_hospitalizacion):
+    response = client.post("/api/hospitalizacion/atencion", json={
+        "id_doctor": 99999,
+        "id_diagnostico": 1,
+        "id_hospitalizacion": crear_hospitalizacion.id_hospitalizacion,
+        "observaciones": "Observaciones de prueba",
+        "tratamiento": "Tratamiento de prueba"
+    })
+    assert response.status_code == 400
+    assert response.json()["message"] == "El doctor no existe"
+
+def test_crear_atencion_hospitalizacion_diagnostico_no_existe(client, crear_hospitalizacion, crear_doctor):
+    response = client.post("/api/hospitalizacion/atencion", json={
+        "id_doctor": 12345,
+        "id_diagnostico": 999,
+        "id_hospitalizacion": crear_hospitalizacion.id_hospitalizacion,
+        "observaciones": "Observaciones de prueba",
+        "tratamiento": "Tratamiento de prueba"
+    })
+    assert response.status_code == 400
+    assert response.json()["message"] == "El diagnóstico no existe"
+
+def test_crear_atencion_hospitalizacion_hospitalizacion_no_existe(client, crear_doctor, crear_diagnostico):
+    response = client.post("/api/hospitalizacion/atencion", json={
+        "id_doctor": 12345,
+        "id_diagnostico": 1,
+        "id_hospitalizacion": 999,
+        "observaciones": "Observaciones de prueba",
+        "tratamiento": "Tratamiento de prueba"
+    })
+    assert response.status_code == 400
+    assert response.json()["message"] == "La hospitalización no existe"
+
+def test_crear_atencion_hospitalizacion_exitoso(client, crear_hospitalizacion, crear_doctor, crear_diagnostico):
+    response = client.post("/api/hospitalizacion/atencion", json={
+        "id_doctor": 12345,
+        "id_diagnostico": 1,
+        "id_hospitalizacion": crear_hospitalizacion.id_hospitalizacion,
+        "observaciones": "Observaciones de prueba",
+        "tratamiento": "Tratamiento de prueba"
+    })
+    assert response.status_code == 201
+    assert response.json()["data"]["id_doctor"] == 12345
+    assert response.json()["data"]["id_diagnostico"] == 1
+    assert response.json()["data"]["id_hospitalizacion"] == crear_hospitalizacion.id_hospitalizacion
+
+def test_obtener_atenciones_hospitalizacion_no_existe(client):
+    response = client.get("/api/hospitalizacion/atencion/999")
+    assert response.status_code == 400
+    assert response.json()["message"] == "La hospitalización no existe"
+
+def test_obtener_atenciones_hospitalizacion_exitoso(client, crear_hospitalizacion, crear_atencion_hospitalizacion):
+    response = client.get(f"/api/hospitalizacion/atencion/{crear_hospitalizacion.id_hospitalizacion}")
+    assert response.status_code == 200
+    assert "data" in response.json()
+    assert len(response.json()["data"]) > 0
